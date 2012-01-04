cccde72d4c4bcfa461f8e87961a34e190e6572cf
Boris Valerstein <borisv@aweber.com>
2012-01-04 13:37:14 -0500

    Made custom_fields DataDict inherit from dict

--- aweber_api/data_dict.py
+++ aweber_api/data_dict.py
@@ -4,16 +4,18 @@ used for when an AWeberEntry has a dict item as on of the attributes
 in _data.  When changes are made to an item in this data dict, __setattr__
 gets called on the parent with the new state of the dict.
 """
-class DataDict(object):
+class DataDict(dict):
 
     def __init__(self, data, name, parent):
+        super(DataDict, self).__init__(data)
         self.parent = parent
-        self.data = data
         self.name = name
 
-    def __getitem__(self, key):
-        return self.data[key]
-
     def __setitem__(self, key, value):
-        self.data[key] = value
+        super(DataDict, self).__setitem__(key, value)
         self.parent.__setattr__(self.name, self.data)
+
+    #Backwards compatible - do not remove
+    @property
+    def data(self):
+        return self
